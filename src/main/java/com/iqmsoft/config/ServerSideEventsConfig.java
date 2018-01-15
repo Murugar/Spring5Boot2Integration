d3f0c1ff95da805292482196eeaba2398f46a469
Davanon <davanon2014@gmail.com>
2018-01-15 18:41:48 +0530

    First

---
+++ src/main/java/com/iqmsoft/config/ServerSideEventsConfig.java
@@ -0,0 +1,23 @@
+package com.iqmsoft.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.http.dsl.Http;
+import reactor.core.publisher.Flux;
+
+@Configuration
+public class ServerSideEventsConfig {
+
+  @Bean
+  public IntegrationFlow sseFlow() {
+
+    return IntegrationFlows.from(Http.inboundReactiveGateway("/test")
+                                     .requestMapping(m -> m.produces(MediaType.TEXT_EVENT_STREAM_VALUE)))
+                           .handle((p, h) -> Flux.just("foo", "bar", "baz", "for"))
+                           .get();
+  }
+
+}
